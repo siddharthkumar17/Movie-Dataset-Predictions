1492ccc78a01139379afd235474ea2f401169ddb
Siddharth Kumar <sidkumar@live.com>
2020-04-12 13:07:44 -0500

    Update movie_analysis.scala

--- movie_analysis.scala
+++ movie_analysis.scala
@@ -1,9 +1,16 @@
 import org.apache.spark.sql._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import org.apache.spark.sql.functions._
+
 
 val spark: SparkSession = SparkSession.builder.appName("movies").getOrCreate()
 
-val df: DataFrame = spark.read.csv("/FileStore/tables/links.csv")
-df.show()
+val credits: DataFrame = spark.read.format("csv") .option("header", "true").load("/FileStore/tables/credits.csv")
+val ratings: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/ratings.csv")
+val keywords: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/keywords.csv")
+val movies_metadata: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/movies_metadata.csv")
+val merged = credits.join(ratings, credits("id")===ratings("movieId"), "inner").join(keywords,credits("id")===keywords("id"), "inner" ).join(movies_metadata, credits("id")===movies_metadata("id"),"inner")
 
+merged.show()
+merged.printSchema()
