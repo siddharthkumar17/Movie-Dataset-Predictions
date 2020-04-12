c206bac72bed51bd01be0eb2dd9b6a4bce0a8e4b
Siddharth Kumar <sidkumar@live.com>
2020-04-12 18:53:41 -0500

    Update movie_analysis.scala

--- movie_analysis.scala
+++ movie_analysis.scala
@@ -4,7 +4,7 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.sql.functions._
 import com.google.gson._
 import org.apache.spark.sql.types._
-
+import scala.collection.mutable._
 def cleanString(input: String): String = {
 
   return input.replace('\'','\"').replace("None","\"\"")
@@ -14,12 +14,12 @@ def cleanString(input: String): String = {
 val spark: SparkSession = SparkSession.builder.appName("movies").getOrCreate()
 
 val credits: DataFrame = spark.read.format("csv") .option("header", "true").load("/FileStore/tables/credits.csv")
-val ratings: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/ratings.csv")
+val ratings: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/ratings.csv").drop("userId", "timestamp").groupBy("movieId").agg(avg("rating").as("avg_rating")).withColumnRenamed("movieId","id")
 val keywords: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/keywords.csv")
 val movies_metadata: DataFrame = spark.read.format("csv").option("header", "true").load("/FileStore/tables/movies_metadata.csv")
-var merged = credits.join(ratings, ratings("movieId")===credits("id"), "inner").join(keywords,Seq("id"), "inner" ).join(movies_metadata, Seq("id"),"inner")
-merged = merged.drop("movieId","userId","tagline","spoken_languages", "vote_count", "vote_average", "homepage", "imdb_id", "production_countries", "poster_path", "original_title", "timestamp")
-merged = merged.withColumn("rating", merged("rating").cast(DoubleType)).withColumn("budget", merged("budget").cast(LongType))
+var merged = credits.join(ratings, Seq("id"), "inner").join(keywords,Seq("id"), "inner" ).join(movies_metadata, Seq("id"),"inner")
+merged = merged.drop("tagline","spoken_languages", "vote_count", "vote_average", "homepage", "imdb_id", "production_countries", "poster_path", "original_title")
+merged = merged.withColumn("budget", merged("budget").cast(LongType))
 def extractActorList(cast: String): Array[String] ={
   val temp = cleanString(cast)
   var res: String = ""
@@ -32,20 +32,18 @@ def extractActorList(cast: String): Array[String] ={
 val udf1 = udf((x: String) => extractActorList(x))
 merged = merged.withColumn("cast",udf1($"cast"))
 
-def extractCrew(crew: String): Array[String] ={
+def extractCrew(crew: String): Map[String,String] ={
   val temp = cleanString(crew)
-  var res: String = ""
+  var res = new HashMap[String, String]
   val json = new Gson().fromJson(temp, classOf[JsonArray]);
   for(t<-0 to json.size()-1){
     val a = json.get(t).getAsJsonObject()
-    res=res+a.get("job").getAsString() +'-'+ a.get("name").getAsString().replace("\"", "").replace(" ","-")+' '
+    res+=(a.get("job").getAsString() -> a.get("name").getAsString().replace("\"", "").replace(" ","-"))
   }
-  res = res.trim()
   
-  return res.split(" ")
+  
+  return res
 }
 
 val udf2 = udf((x: String) => extractCrew(x))
 merged = merged.withColumn("crew",udf2($"crew"))
-
-print(merged.first())
